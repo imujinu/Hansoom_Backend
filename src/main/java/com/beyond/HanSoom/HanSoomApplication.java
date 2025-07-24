8cba91fb6d4971352133376f91b755d01cf211a1
EunDuk2 <dmsqls6789@naver.com>
2025-07-24 23:29:23 +0900

    프로젝트 생성

---
+++ src/main/java/com/beyond/HanSoom/HanSoomApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.HanSoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanSoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanSoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyond/HanSoom/HanSoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.HanSoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanSoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
