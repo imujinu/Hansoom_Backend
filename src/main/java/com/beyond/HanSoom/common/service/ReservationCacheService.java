40957b197168f45f2897b17ea60d6e6b6d3eee1c
imujinu <wlsdnrhdwkd@gmail.com>
2025-08-11 17:54:19 +0900

    [250811]Faet : 예약 캐싱

--- src/main/java/com/beyond/HanSoom/common/service/ReservationCacheService.java
+++ src/main/java/com/beyond/HanSoom/common/service/ReservationCacheService.java
@@ -1,8 +1,14 @@
 package com.beyond.HanSoom.common.service;
 
+import com.beyond.HanSoom.reservation.domain.Reservation;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 
@@ -11,5 +17,25 @@ public class ReservationCacheService {
 
     public ReservationCacheService(@Qualifier("bookingCacheInventory") RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
+    }
+
+    public Optional<Reservation> getCacheReservation(Long reservationId) throws JsonProcessingException {
+
+        Object cacheReservation = redisTemplate.opsForValue().get(reservationId);
+        if(cacheReservation==null){
+            return Optional.empty();
+        }else{
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Reservation reservation1 = objectMapper.readValue(cacheReservation.toString(), Reservation.class);
+        return Optional.of(reservation1);
+        }
+    }
+
+    public void saveCacheReservation(Reservation reservation) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String cacheReservation = objectMapper.writeValueAsString(reservation);
+        redisTemplate.opsForValue().set(String.valueOf(reservation.getId()), cacheReservation);
     }
 }
