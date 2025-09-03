71e3e7043cc1c39531eeec2aabb80859882bf11b
imujinu <wlsdnrhdwkd@gmail.com>
2025-09-03 19:09:46 +0900

    [250903]Feat:채팅 호스트 공지

--- src/main/java/com/beyond/HanSoom/chat/domain/ChatAnnouncement.java
+++ src/main/java/com/beyond/HanSoom/chat/domain/ChatAnnouncement.java
@@ -1,24 +1,27 @@
 package com.beyond.HanSoom.chat.domain;
 
+import com.beyond.HanSoom.common.domain.BaseTimeEntity;
+import com.beyond.HanSoom.hotel.domain.Hotel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ChatAnnouncement {
+public class ChatAnnouncement  extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="chat_room_id", nullable = false)
-    private ChatRoom chatRoom;
+    @JoinColumn(name="hotel_id", nullable = false)
+    private Hotel hotel;
 
     @Column(nullable = false, length = 500)
     private String title;
@@ -26,4 +29,10 @@ public class ChatAnnouncement {
     @Column(nullable = false, length = 500)
     private String content;
     private String isActive;
+    private String chatType;
+
+    public void changeActive(String isActive){
+        this.isActive = isActive;
+    }
+
 }
